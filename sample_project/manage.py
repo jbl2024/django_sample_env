3de5edda4eb816d5da9263742d524b64479c8546
Jérôme Blondon <jerome@yasound.com>
2012-10-14 17:49:41 +0200

    updated manage.py for django 1.4
    see https://docs.djangoproject.com/en/dev/releases/1.4/#updated-default-project-layout-and-manage-py for more information

--- sample_project/manage.py
+++ sample_project/manage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
-import sys
-
 import os
+import sys
 from os.path import abspath, dirname, join
 
 PROJECT_ROOT = abspath(dirname(__file__))
@@ -15,7 +14,7 @@ sys.path.insert(0, join(PROJECT_ROOT, "apps"))
 
 # Normal django...
 
-if not os.environ.has_key('DJANGO_MODE'): 
+if 'DJANGO_MODE' not in os.environ:
     os.environ['DJANGO_MODE'] = 'local'
     warning = "------------------------------------------------------------------------------\n"
     warning += " Manage in LOCAL mode : set DJANGO_MODE env variable for prod and dev servers.\n"
@@ -25,13 +24,10 @@ if not os.environ.has_key('DJANGO_MODE'):
     #from time import sleep
     #sleep(1)
 
-from django.core.management import execute_manager
-
-try:
-    import settings # Assumed to be in the same directory.
-except ImportError:
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-    sys.exit(1)
 
 if __name__ == "__main__":
-    execute_manager(settings)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
